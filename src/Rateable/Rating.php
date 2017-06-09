8b9c55df7b466b2fb149beeb102e1224119bdee1
Raj Janorkar <raj.janorkar@gmail.com>
2017-06-10 00:10:19 +1000

    Fixed the issue for user function

--- src/Rateable/Rating.php
+++ src/Rateable/Rating.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model
 {
+
     /**
      * The attributes that are mass assignable.
      *
@@ -27,6 +28,7 @@ class Rating extends Model
      */
     public function user()
     {
-        return $this->belongsTo(Config::get('auth.model'));
+        return $this->belongsTo(Config::get('auth.providers.users.model'));
     }
+
 }
