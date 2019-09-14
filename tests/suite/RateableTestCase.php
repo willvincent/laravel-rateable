8f2062ed9cdfe0a9b9c17e3b255312765cb02f9e
Vu Duy Khanh <khanh_vu14@yahoo.com>
2019-09-14 23:26:05 +0700

    Update RateableTestCase.php

--- tests/suite/RateableTestCase.php
+++ tests/suite/RateableTestCase.php
@@ -4,14 +4,14 @@ use PHPUnit\Framework\TestCase;
 class RateableTestCase extends TestCase
 {
 
-    public static function setUpBeforeClass()
+    public static function setUpBeforeClass(): void
     {
         with(new UserMigrator())->up();
         with(new PostMigrator())->up();
         with(new RatingMigrator())->up();
     }
 
-    public function setUp()
+    public function setUp(): void
     {
         with(new UserSeeder())->run();
         with(new PostSeeder())->run();
