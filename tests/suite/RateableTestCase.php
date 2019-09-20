17ee71c291474c8d8aee68192ffa5364d60642d6
Will Vincent <will@willvincent.com>
2019-09-20 11:56:34 -0500

    Update to latest phpunit

--- tests/suite/RateableTestCase.php
+++ tests/suite/RateableTestCase.php
@@ -1,16 +1,18 @@
 <?php
 
-class RateableTestCase extends PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class RateableTestCase extends TestCase
 {
 
-    public static function setUpBeforeClass()
+    public static function setUpBeforeClass() : void
     {
         with(new UserMigrator())->up();
         with(new PostMigrator())->up();
         with(new RatingMigrator())->up();
     }
 
-    public function setUp()
+    public function setUp() : void
     {
         with(new UserSeeder())->run();
         with(new PostSeeder())->run();
