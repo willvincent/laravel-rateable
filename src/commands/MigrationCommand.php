cc11d9fe41070e0a1cbe9307c46d96cdf8fd002e
Will Vincent <will@willvincent.com>
2019-09-20 12:15:11 -0500

    Prevent duplication of migration file with different timestamps

--- src/commands/MigrationCommand.php
+++ src/commands/MigrationCommand.php
@@ -61,14 +61,16 @@ class MigrationCommand extends Command
      */
     protected function createMigration()
     {
-        $migration_file = database_path('migrations').'/'.date('Y_m_d_His').'_create_ratings_table.php';
+      $migration_file = database_path('migrations').'/'.date('Y_m_d_His').'_create_ratings_table.php';
 
-        if (! file_exists($migration_file) && $fs = fopen($migration_file, 'x')) {
-            fwrite($fs, file_get_contents(__DIR__.'/../migrations/create_ratings_table.php'));
-            fclose($fs);
-            return true;
-        }
+      $existing = glob(database_path('migrations').'/*_create_ratings_table.php');
 
-        return false;
-    }
+      if (empty($existing) && $fs = fopen($migration_file, 'x')) {
+          fwrite($fs, file_get_contents(__DIR__.'/../migrations/create_ratings_table.php'));
+          fclose($fs);
+          return true;
+      }
+
+      return false;
+  }
 }
